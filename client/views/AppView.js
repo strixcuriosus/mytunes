17838fe197f2da6517a7b69bcac2bc5b62a00e9e
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-30 23:15:33 -0700

    Started implementing playlist queue view.

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -4,18 +4,27 @@ var AppView = Backbone.View.extend({
   initialize: function(params){
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
+    this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
-    this.model.on('change:currentSong', function(model){
-      this.playerView.setSong(model.get('currentSong'));
+    this.model.on({
+
+      'change:currentSong': function(model){
+        this.playerView.setSong(model.get('currentSong'));
+      },
+
+      'change:songQueue': function(model){
+        this.songQueueView.render();
+      }
     }, this);
   },
 
   render: function(){
     return this.$el.html([
       this.playerView.$el,
-      this.libraryView.$el
+      this.libraryView.$el,
+      this.songQueueView.$el
     ]);
   }
 
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -3,14 +3,18 @@ var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
+    this.collection.on('change', function(){
+      console.log('queue collection changed');
+      this.render();
+    },this );
   },
 
   render: function() {
     this.$el.children().detach();
 
-    this.$el.html('<th>Playlist</th>').append(
-      this.collection.map(function(song){
-        return new SongQueueEntryView({model: song}).render();
+    return this.$el.html('<th>Playlist</th>').append(
+           this.collection.map(function(song){
+            return new SongQueueEntryView({model: song}).render();
       })
     );
 
